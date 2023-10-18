a05ec385ca00058f79fc16bdc5e146d41e74be01
metolone <metoronexyz@gmail.com>
2023-10-18 22:07:35 +0900

    ステップ22まで完了

---
+++ steps/step21.py
@@ -0,0 +1,269 @@
+import numpy as np
+import unittest
+import weakref
+import contextlib
+
+
+
+class Variable:
+    def __init__(self, data, name=None):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+
+
+        self.data = data
+        self.name = name #変数に名前を設定できるように
+        self.grad = None #微分した値
+        self.creator = None #関数の出力結果
+        self.generation = 0 #世代を記録（処理の優先度に対応）
+
+        Variable.__mul__ = mul
+        Variable.__add__ = add
+        Variable.__rmul__ = mul
+        Variable.__radd__ = add
+        Variable.__neg__ = neg
+        Variable.__sub__ = sub
+        Variable.__rsub__ = rsub
+        Variable.__truediv__ = div
+        Variable.__rtruediv__ = rdiv
+        Variable.__pow__ = pow
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def cleargrad(self):
+        self.grad = None
+
+    #retain_gradがTrueのときすべての変数が勾配を保存
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data) #dy/dyの自動追加
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+        while funcs:
+            f = funcs.pop() #関数を取得
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)#複数の関数の入出力値を取得
+            if not isinstance(gxs, tuple):
+                gxs = (gxs, )
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+
+    @property #インスタンス変数としてアクセス
+    def shape(self):
+        return self.data.shape
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    #オーバーロード
+    #len関数が使えるように
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' *9)
+        return 'variable(' + p + ')'
+
+
+
+def as_array(x):
+    if np.isscalar(x): #スカラ系の型を判定
+        return np.array(x)
+    return x
+
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+
+class Function:
+    #アスタリスク・・・可変長引数
+    def __call__(self, *inputs):
+        inputs = [as_variable(x) for x in inputs]
+
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys] #リスト内包表記
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = [weakref.ref(output) for output in outputs] #弱参照を作り循環をなくす
+        return outputs if len(outputs) > 1 else outputs[0]
+    def forward(self, xs):
+        raise NotImplementedError() #まだ実装されていない部分を表す
+    #逆伝播
+    def backward(self, gys):
+        raise NotImplementedError()
+
+#インスタンス化しない
+class Config:
+    enable_backprop = True
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy * x1, gy * x0
+
+def mul(x0, x1):
+    return Mul()(x0, x1)
+#継承
+class Square(Function):
+    def forward(self, x):
+        return x**2
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2*x*gy #gyは出力側から伝わる微分が渡される。
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x)*gy
+        return gx
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
+def add(x0, x1):
+    x1 = as_array(x1)
+    return Add()(x0, x1)
+
+class Neg(Function):
+    def forward(self, x):
+        return -y
+
+    def backward(self, gy):
+        return -gy
+
+def neg(x):
+    return Neg()(x)
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 -x1
+        return y
+
+    def backward(self, gy):
+        return gy, -gy
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+class Pow(Function):
+    def __int__(self, C):
+        self.C = C
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c*x ** (c - 1)*gy
+        return gx
+
+def pow(x, c):
+    return Pow(c)(x)
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0/ x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy*(-x0/x1 ** 2)
+        return gx0, gx1
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+
+#中心差分近似を用いて数値微分を求める関数を実装する
+def numerical_diff(f, x, eps = 1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+def no_grad():
+    return using_config('enable_backprop', False)
+
+
+x = Variable(np.array(2.0))
+y = x + 3.0
+print(y)

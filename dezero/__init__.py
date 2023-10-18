7e57f9dd73c40991907285c8f88d0c1130e41b46
metolone <metoronexyz@gmail.com>
2023-10-18 23:13:08 +0900

    ステップ26まで完了

---
+++ dezero/__init__.py
@@ -0,0 +1,14 @@
+# =============================================================================
+# step23.pyからstep32.pyまではsimple_coreを利用
+is_simple_core = False  # True
+# =============================================================================
+
+from dezero.core_simple import Variable
+from dezero.core_simple import Function
+from dezero.core_simple import using_config
+from dezero.core_simple import no_grad
+from dezero.core_simple import as_array
+from dezero.core_simple import as_variable
+from dezero.core_simple import setup_variable
+
+setup_variable()

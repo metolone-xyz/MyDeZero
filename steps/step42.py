74dfb29527afe925290269ec0e75b802d944d972
metolone <metoronexyz@gmail.com>
2023-10-20 13:04:39 +0900

    線形回帰を実装

---
+++ steps/step42.py
@@ -0,0 +1,45 @@
+import numpy as np
+from dezero import Variable
+import dezero.functions as F
+import matplotlib.pyplot as plt
+
+#トイ・データセット
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = 5 + 2*x + np.random.rand(100, 1)
+
+x, y = Variable(x), Variable(y)
+
+W = Variable(np.zeros((1, 1)))
+b = Variable(np.zeros(1))
+
+def predict(x):
+    y = F.matmul(x, W) + b #100個のデータに対してまとめて計算
+    return y
+
+def mean_squared_error(x0, x1):
+    diff = x0 - x1
+    return F.sum(diff**2)/len(diff)
+
+
+
+lr = 0.1
+iters = 100
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = mean_squared_error(y, y_pred)
+
+    W.cleargrad()
+    b.cleargrad()
+    loss.backward()
+
+    W.data -= lr*W.grad.data
+    b.data -= lr*b.grad.data
+    print(W, b, loss)
+
+
+plt.scatter(x.data, y.data, label='True data')
+plt.plot(x.data, predict(x).data, color='red', label='Prediction after training')
+plt.legend()
+plt.show()
